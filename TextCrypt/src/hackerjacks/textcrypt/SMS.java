2f41f8841ce9e7d4201de60b33755eff26f1485d
monkifiery <monkifiery@gmail.com>
2014-03-22 20:47:29 -0400

    Ajay Benno 6 - Added Contact Chooser

--- TextCrypt/src/hackerjacks/textcrypt/SMS.java
+++ TextCrypt/src/hackerjacks/textcrypt/SMS.java
@@ -6,7 +6,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.gsm.SmsManager;
 import android.view.View;
 import android.widget.Button;
@@ -53,9 +56,38 @@ public class SMS extends Activity
         });        
     }
     public void displayContacts(View v){
-    Intent intent= new Intent(this, ChooseContacts.class);
-    startActivity(intent);
+    	Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        // BoD con't: CONTENT_TYPE instead of CONTENT_ITEM_TYPE
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+        startActivityForResult(intent, 1);  
     }
+    
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            Uri uri = data.getData();
+
+            if (uri != null) {
+                Cursor c = null;
+                try {
+                    c = getContentResolver().query(uri, new String[]{ 
+                                ContactsContract.CommonDataKinds.Phone.NUMBER,  
+                                ContactsContract.CommonDataKinds.Phone.TYPE },
+                            null, null, null);
+
+                    if (c != null && c.moveToFirst()) {
+                        String number = c.getString(0);
+                        int type = c.getInt(1);
+                        txtPhoneNo.setText(number);
+                    }
+                } finally {
+                    if (c != null) {
+                        c.close();
+                    }
+                }
+            }
+        }
+    }
+
     
     //---sends a SMS message to another device---
     private void sendSMS(String phoneNumber, String message)
