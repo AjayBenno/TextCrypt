463aa88209eb9441c95ace5bd75d86206e5b76e5
Varun Belur <varunbelur78@gmail.com>
2014-03-23 00:12:09 -0400

    cleaned3

--- TextCrypt/src/hackerjacks/textcrypt/Decrypt.java
+++ TextCrypt/src/hackerjacks/textcrypt/Decrypt.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 package hackerjacks.textcrypt;
 
@@ -22,43 +22,3 @@ public class Decrypt extends Activity {
 	}
 
 }
-=======
-package hackerjacks.textcrypt;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-public class Decrypt extends Activity {
-	EditText enterDecrypt;
-	TextView showDecryption;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_decrypt);
-		enterDecrypt = (EditText) findViewById(R.id.enterDecrypt);
-		showDecryption =(TextView) findViewById(R.id.showDecryption);		
-	}
-	public void decryptn(View v){
-		String decryption = enterDecrypt.getText().toString();
-		int length = decryption.length() /2;
-		String key = decryption.substring(length);
-		String message = decryption.substring(0,length);
-		showDecryption.setText(OneTimePad.decrypt(message, key));
-	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.decrypt, menu);
-		return true;
-	}
-
-}
-
-
-
->>>>>>> 948dd5438f9471677555c0d5209a7cf6f314976d
