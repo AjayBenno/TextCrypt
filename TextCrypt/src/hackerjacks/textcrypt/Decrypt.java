b157b1e3c6e96e90d85e8389687a3ef799827149
Varun Belur <varunbelur78@gmail.com>
2014-03-22 22:58:23 -0400

    test4

---
+++ TextCrypt/src/hackerjacks/textcrypt/Decrypt.java
@@ -0,0 +1,72 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+package hackerjacks.textcrypt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Decrypt extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_decrypt);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.decrypt, menu);
+		return true;
+	}
+
+}
+=======
+package hackerjacks.textcrypt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Decrypt extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_decrypt);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.decrypt, menu);
+		return true;
+	}
+
+}
+>>>>>>> 4e4b49284ec7347849c85b5f676e3a5ff80ff7e4
+=======
+package hackerjacks.textcrypt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Decrypt extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_decrypt);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.decrypt, menu);
+		return true;
+	}
+
+}
+>>>>>>> 4e4b49284ec7347849c85b5f676e3a5ff80ff7e4
